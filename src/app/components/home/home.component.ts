6dc024b8e886b8bbfb77fe99948ba5d39616fb0f
ROHIT <rohitgambhir96@gmail.com>
2020-07-17 00:27:28 +0530

    home component with addToCart

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Product} from './../../interface/products.interface';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  Products: Array<Product> =[
+ {
+   Name : "Banana",Price: 3,Desc:"Fruit" , ProductPath:'assets/banana.jpg'
+ },
+ {
+  Name : "Mango",Price: 4,Desc:"Fruit" , ProductPath:'/assets/mango.png'
+},
+{
+  Name : "kiwi",Price: 2,Desc:"Fruit" , ProductPath:'assets/kiwi.jpg'
+},
+{
+  Name : "strawberry",Price: 1,Desc:"Fruit" , ProductPath:'assets/strawberry.jpeg'
+},
+
+  ];
+     
   constructor() { }
 
   ngOnInit(): void {
+  }
+  addToCart(index)
+  {
+    console.log("Added" , this.Products[index]);
   }
 
 }
